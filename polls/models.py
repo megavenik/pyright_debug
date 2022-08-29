6216d1d09e55da06cbb40b4163a17f4b8aac1132
Dmitry Chumak <mega.venik@gmail.com>
2022-08-29 18:14:22 +0300

    models examples added

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Tip(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048)
+    code = models.TextField(max_length=2048)
+    source = models.URLField(max_length=256, null=True, default=None)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.title
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from polls.models import Tip
 
 # Create your views here.
 
 def main(request):
+    all = Tip.objects.all()
+    Tip.ti
+    for i in all:
+        print(i.)
     if request.use
